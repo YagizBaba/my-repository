01838aeb1be844e3a2faa7525fdde771ce52f62c
YagizBaba <yaggiz@gmail.com>
2024-08-19 21:02:54 -0400

    Add files via upload

---
+++ 101-kittens-carousel-static-website-ec2/setup.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+yum update -y
+yum install httpd -y
+
+cd /var/www/html
+
+FOLDER=https://raw.githubusercontent.com/YagizBaba/my-repository/main/static-web
+
+wget ${FOLDER}/index.html 
+wget ${FOLDER}/cat0.jpg
+wget ${FOLDER}/cat1.jpg
+wget ${FOLDER}/cat2.jpg
+
+systemctl start httpd
+systemctl enable httpd
