8c3b78b1eb9f2dac809a24ee80f713469984c48e
yagiz <yagizerisen@hotmail.com>
2024-09-09 18:20:24 -0400

    kediler

--- 101-kittens-carousel-static-website-ec2/setup.sh
+++ 101-kittens-carousel-static-website-ec2/setup.sh
@@ -5,7 +5,7 @@ yum install httpd -y
 
 cd /var/www/html
 
-FOLDER=https://raw.githubusercontent.com/YagizBaba/my-repository/main/static-web
+FOLDER=https://raw.githubusercontent.com/YagizBaba/my-repository/main/101-kittens-carousel-static-website-ec2/static-web
 
 wget ${FOLDER}/index.html 
 wget ${FOLDER}/cat0.jpg
